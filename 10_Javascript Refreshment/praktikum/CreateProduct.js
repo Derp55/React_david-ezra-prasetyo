a7b3052a9a8c145b380776c47cbf371583aa09e9
David Ezra <2020104954@student.kalbis.ac.id>
2023-03-05 03:55:57 +0700

    tugas

---
+++ 10_Javascript Refreshment/praktikum/CreateProduct.js
@@ -0,0 +1,48 @@
+document.getElementById("selesai").addEventListener("click", function () {
+    const productName = document.getElementById("namaproduk").value;
+    const productCategory = document.getElementById("kategoriproduk").value;
+    const imageProduct = document.getElementById("gambarproduk").value;
+    const productFreshness = document.querySelector(
+      'input[name="kondisiproduk"]:checked'
+    );
+    const additionalDescription = document.getElementById(
+      "deskripsiproduk"
+    ).value;
+    const productPrice = document.getElementById("hargaproduk").value;
+  
+    if (productName == "" || productPrice == "") {
+      alert("Please enter a valid Product name and price.");
+    } else {
+      if (productName.length > 25) {
+        alert("Product Name must not exceed 25 characters.");
+      }
+    }
+  
+    if (productName === "") {
+      alert("The Product Name field must be filled in.");
+      return;
+    } else if (/[**@/#{}**]/.test(productName)) {
+      alert("Name must not contain symbols.");
+      return;
+    }
+  
+    if (productCategory === "") {
+      alert("The Product Category field must be filled in.");
+      return;
+    }
+    if (imageProduct === "") {
+      alert("The Image of Product field must be filled in.");
+      return;
+    }
+    if (!productFreshness) {
+      alert("The Product Freshness field must be filled in.");
+      return;
+    }
+    if (additionalDescription === "") {
+      alert("The Additional Description field must be filled in.");
+      return;
+    }
+    if (productPrice === "") {
+      alert("The Product Price field must be filled in.");
+      return;
+}})
