0b53f529f7c1ffc135ddb7ed23ed72e7ac3e4345
David Ezra Prasetyo <2020104954@student.kalbis.ac.id>
2023-03-19 06:57:33 +0700

    Tugas

--- 15_Event Handling/praktikum/createproduct/src/component/body.jsx
+++ 15_Event Handling/praktikum/createproduct/src/component/body.jsx
@@ -1,22 +1,46 @@
 import './body.css';
-import React from 'react';
-import Logo from './bootstrap-logo';
+import React, { useState } from 'react';
+import Logo from '../assets/bootstrap-logo.svg';
 
 function Body() {
+const [ProductName, setProductName] = useState('');
+
+const article = {
+  title: {
+    id: "Buat Produk",
+    en: "Create Product"
+  },
+  description: {
+    id: "Di bawah ini adalah contoh formulir yang dibuat seluruhnya dengan kontrol formulir Bootstrap. Setiap grup formulir yang diperlukan memiliki status validasi yang dapat dipicu dengan mencoba mengirimkan formulir tanpa menyelesaikannya.",
+    en: "Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it."
+  }
+};
+const handleClick = (e) => {
+    e.preventDefault();
+    const randomNumber = Math.floor(Math.random() * 100);
+   console.log(randomNumber);
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+
+    if (value.length <= 10) {
+      setProductName(value);
+    }
+  };
+
     return (
         <div className='body'>
             <img src={Logo} alt="logo" />
-            <h1>Create Product</h1>
-            <p>Below is an example form built entirely with Bootstrap’s form controls. 
-            Each required form group has a validation state
-            that can be triggered by attempting to submit the form without completing it.</p>
+            <h1>{article.title.en}</h1>
+            <p>{article.description.en}</p>
 
             <form action="">
             <h3>Detail Product</h3>
 
             <div className='col1'>
             <label htmlFor="">Product Name</label>
-            <input type="text" />
+            <input type="text" value={ProductName} onChange={handleInputChange}/>
             </div>
 
             <div>
@@ -57,8 +81,11 @@ function Body() {
             </form>
            
            <input type="submit" value="Submit" />
+           <button onClick={handleClick}>Generate random number</button>
+
         </div>
+       
     )
-}
+    } 
 
 export default Body;
---
+++ 18_React Routing/praktikum/tugas/src/landing.jsx
@@ -0,0 +1,15 @@
+import Body from "./component/Apps/LandingPage/body.landing";
+import Header from "./component/Apps/LandingPage/header.landing";
+import Footer from "./component/Apps/LandingPage/footer.landing";
+
+function Landing() {
+  return (
+    <div className="App">
+    <Header/>
+    <Body/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default Landing;
